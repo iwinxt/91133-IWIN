0e1aa1ba45180797e426a33c925092f6d4559fa4
iwinxt <iwinlimab@gmail.com>
2025-05-07 07:54:55 -0300

    update

--- app/View/listar.php
+++ app/View/listar.php
@@ -6,29 +6,30 @@ include 'conecta.php';
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Controle de Estoque 1.0 - SENAI FSA</title>
+    <title>Controle de Estoque 1.0 - SENAI FFSA</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"> 
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
 <body>
-    
-    <h1>Controle de Estoque - SENAI FSA</h1>
+
+    <h1>Controle de Estoque 1.0 - SENAI FFSA</h1>
+
     <?php
     $sql = "SELECT * FROM produtos ORDER BY id DESC";
     $resultado = $conn->query($sql);
 
-    if ($resultado->num_rows > 0){
+    if ($resultado->num_rows > 0) {
         echo "<table>";
         echo "<thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Produto</th>
-                        <th>Tipo</th>
-                        <th>Quantidade</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>";
+                <tr>
+                    <th>ID</th>
+                    <th>Produto</th>
+                    <th>Tipo</th>
+                    <th>Quantidade</th>
+                    <th>Ações</th>
+                </tr>
+              </thead>";
         echo "<tbody>";
 
         while ($linha = $resultado->fetch_assoc()) {
@@ -38,19 +39,28 @@ include 'conecta.php';
             echo "<td>" . htmlspecialchars($linha['tipo']) . "</td>";
             echo "<td>" . htmlspecialchars($linha['quantidade']) . "</td>";
             echo "<td>
+
+
+
+
                     <a href='editar.php?id=" . $linha['id'] . "' title='Editar'>
                         <i class='fas fa-edit' style='color:blue;'></i>
                     </a>
                     &nbsp;&nbsp;
+
+
+
+
                     <a href='excluir.php?id=" . $linha['id'] . "' title='Excluir' onclick=\"return confirm('Deseja realmente excluir este produto?');\">
                         <i class='fas fa-trash-alt' style='color:red;'></i>
                     </a>
-                </td>";
+                  </td>";
             echo "</tr>";
         }
-        echo"</tbody>";
-        echo"</table>";
-    } else{
+
+        echo "</tbody>";
+        echo "</table>";
+    } else {
         echo "<p>Nenhum produto cadastrado.</p>";
     }
 
@@ -62,5 +72,6 @@ include 'conecta.php';
             <button>Voltar ao Cadastro</button>
         </a>
     </div>
+
 </body>
 </html>
